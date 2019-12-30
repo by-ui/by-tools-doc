5d34863bafbc2aba8b10257f86aa471f79d0584d
肖康正 <452757438@qq.com>
2019-12-30 15:45:12 +0800

    Update routes.ts

--- src/packages/vue-router/routes.ts
+++ src/packages/vue-router/routes.ts
@@ -2,6 +2,11 @@ import { RouteConfig } from 'vue-router';
 
 const routes: RouteConfig[] = [
     {
+        path: '/home',
+        name: 'home',
+        component: () => import(/* webpackChunkName: "modules/home" */ '@/pages/home/home.vue'),
+    },
+    {
         path: '/',
         name: 'index',
         component: () => import(/* webpackChunkName: "modules/index" */ '@/pages/index/index.vue'),
