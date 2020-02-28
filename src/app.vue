e24f9cdef86ada80458ecc30268b911e445e1248
肖康正 <452757438@qq.com>
2020-02-28 14:13:11 +0800

    Update app.vue

--- src/app.vue
+++ src/app.vue
@@ -5,9 +5,14 @@
 </template>
 <script lang="ts">
     import { Vue, Component } from "vue-property-decorator";
+    // import { date } from 'by-tools';
 
     @Component
     export default class App extends Vue {
+        mounted() {
+            // console.log(date.getCurrentWeekDays());
+            // console.log(date.getDateRange('2020-01-01', '2020-01-31'));
+        }
     }
 </script>
 <style lang="scss" scoped>
